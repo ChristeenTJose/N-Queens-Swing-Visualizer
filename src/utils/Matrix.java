9f6aaba09af841c199a0ed2cd5dace2b6a2a3c39
ChristeenTJose <christeenjosethoomkuzhy@gmail.com>
2025-04-19 17:22:47 +0530

    cell painting refactoring

--- src/utils/Matrix.java
+++ src/utils/Matrix.java
@@ -22,19 +22,26 @@ public class Matrix {
     }
 
     public void draw(Graphics2D graphics2D){
-        graphics2D.setStroke(new BasicStroke(4));
+        graphics2D.setStroke(new BasicStroke(5));
         graphics2D.drawRect(x, y, w, h);
         for(int i = 1; i < n; i++){
-            graphics2D.drawLine(x, (int) (y + dY*i), x + w, (int) (y + dY*i));
+            graphics2D.drawLine(x, (int) (y + i*dY), x + w, (int) (y + i*dY));
         }
-        for(int i = 1; i < n; i++){
-            graphics2D.drawLine((int) (x + dX*i), y, (int) (x + dX*i), y + h);
+        for(int j = 1; j < n; j++){
+            graphics2D.drawLine((int) (x + j*dX), y, (int) (x + j*dX), y + h);
         }
         graphics2D.setStroke(new BasicStroke(1));
     }
 
     public void paintCell(Graphics2D graphics2D, int i, int j){
-        graphics2D.fillRect((int) (x + j*dX) + 2, (int) (y + i*dY) + 2, (int) dX - 4, (int) dY - 4);
+        //graphics2D.setPaint(new Color(1, 1, 1, 1));
+        //graphics2D.fillRect((int) (x + j*dX), (int) (y + i*dY), (int) dX, (int) dY);
+        graphics2D.setPaint(new Color(0,0,1,0.9f));
+        graphics2D.fillRect((int) (x + j*dX), (int) (y + i*dY), (int) dX, (int) dY);
+        graphics2D.setStroke(new BasicStroke(5));
+        graphics2D.setPaint(Color.black);
+        graphics2D.drawRect((int) (x + j*dX), (int) (y + i*dY), (int) dX, (int) dY);
+        graphics2D.setStroke(new BasicStroke(1));
     }
 
 }
