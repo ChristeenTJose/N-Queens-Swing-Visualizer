0b9c1875650b025c9257f9f2d5161ab181840e7f
ChristeenTJose <christeenjosethoomkuzhy@gmail.com>
2025-04-19 13:10:08 +0530

    horizontal divisions

--- src/components/VisualizerPanel.java
+++ src/components/VisualizerPanel.java
@@ -12,6 +12,14 @@ public class VisualizerPanel extends JPanel {
     @Override
     public void paint(Graphics graphics){
         Graphics2D graphics2D= (Graphics2D) graphics;
-        graphics2D.drawLine(0,0, this.getWidth(), this.getHeight());
+        hr(graphics2D, 20);
+        hr(graphics2D, 70);
+    }
+
+    private void hr(Graphics2D graphics2D, int percentage){
+        graphics2D.setStroke(new BasicStroke(5));
+        int y = this.getHeight() * percentage/100;
+        graphics2D.drawLine(0, y, this.getWidth(), y);
+        graphics2D.setStroke(new BasicStroke(1));
     }
 }
